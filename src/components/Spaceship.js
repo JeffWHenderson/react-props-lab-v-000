4399425064af47be16df5966f714dc631cc7a2d3
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-26 17:47:13 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -2,10 +2,24 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-let colors = ['black', 'red']
+class Spaceship extends React.Component {
+  render() {
+    return (
+      <div className="spaceship">
+        <h2>Speed: {this.props.name}</h2>
+        <h3>{this.props.speed}</h3>
+        <h4>Rockets?: {this.props.hasRockets}</h4>
+        <h4>Colors: {this.props.colors.join(", ")}</h4>
+      </div>
+    )
+  }
+}
+
 
 SpaceShip.defaultProps = {
+  speed: slow
   hasRockets: false
+  colors: ['black', 'red']
 }
 
 ReactDom.render(
