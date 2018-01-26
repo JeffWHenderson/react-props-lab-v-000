21d62890a8248eaaf01a53e3aea4001d5f8781d1
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-26 17:53:54 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -6,8 +6,8 @@ class Spaceship extends React.Component {
   render() {
     return (
       <div className="spaceship">
-        <h2>Speed: {this.props.name}</h2>
-        <h3>{this.props.speed}</h3>
+        <h2>{this.props.name}</h2>
+        <h3>Speed: {this.props.speed}</h3>
         <h4>Rockets?: {this.props.hasRockets}</h4>
         <h4>Colors: {this.props.colors.join(", ")}</h4>
       </div>
