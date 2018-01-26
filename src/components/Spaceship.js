5606c4cef4aa7e1308571a7e7ec17313e62ac11e
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-26 17:38:32 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -3,6 +3,11 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 let colors = ['black', 'red']
+
+SpaceShip.defaultProps = {
+  hasRockets: false
+}
+
 ReactDom.render(
   <Spaceship name={} />
 )
